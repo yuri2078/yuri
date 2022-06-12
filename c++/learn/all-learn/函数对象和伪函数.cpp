45f129963b0388876897e033e5bf243c62745a0d
yuri2078 <yuri2078170658@gmail.com>
2022-06-12 23:07:16 +0800

    main

---
+++ c++/learn/all-learn/函数对象和伪函数.cpp
@@ -0,0 +1,81 @@
+#include <iostream>
+#include <string>
+
+class Sum
+{
+public:
+    int operator() (int x, int y)
+    {
+        return x + y;
+    }
+};
+
+class Fish
+{
+private:
+    int cout;
+public:
+    Fish()
+    {
+        this->cout = 0;
+    }
+
+    void operator() (void)
+    {
+        std::cout << "鱼儿正在吃东西!\n";
+        this->cout++;
+    }
+
+    int GetCout(void)
+    {
+        return this->cout;
+    }
+};
+
+template <class T>
+class Print
+{
+public:
+    void operator() (const T &s)
+    {
+        std::cout << s << "\n";
+    }
+};
+
+template <class T>
+void ClassPrint(Print<T> &p, const T &s)
+{
+    std::cout << "classprint 打印 ： ";
+    p(s);
+    std::cout  << "\n";
+}
+
+int main(void)
+{
+    //函数对象可以直接用（） 调用
+
+    Sum sum; //生成一个对象
+    std::cout << "sum(10,20) == " << sum(10, 20) << "\n"; //通过对象直接访问函数
+
+    Fish fish; //创建一条鱼
+    //让他吃几次东西
+    fish();
+    fish();
+    fish();
+    fish();
+    std::cout << "鱼儿一共吃了" << fish.GetCout() << "次\n"; //通过对象函数可以访问函数状态，因为他的本质是一个类
+
+    Print<int> p; //建立一个打印整数的对象
+    p(666); //直接打印
+    std::cout << "\n";
+
+    //通过传递对象打印结果
+    Print<std::string> s;
+    std::string ss = "卧槽 太好了";
+    ClassPrint(p, 666);
+    ClassPrint(s, ss);
+    return 0;
+
+    // 总结
+    //仿函数可以有自己状态，可以传递对象打印
+}
---
+++ c++/learn/all-learn/谓词.cpp
@@ -0,0 +1,67 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+
+
+//返回布尔类型的一元运算的仿函数的一元谓词
+class Max_7
+{
+public:
+    bool operator() (int x)
+    {
+        if(x > 7)
+        {
+            return true;
+        }
+        return false;
+    }
+};
+
+class MyCompare
+{
+public:
+    bool operator() (int x, int y)
+    {
+        return x > y;
+    }
+};
+
+int main()
+{
+    std::vector<int> v;
+    for (int i = 0; i < 10;i++)
+    {
+        v.push_back(i*8 % 12);
+    }
+
+    std::vector<int>::iterator choose;
+    choose = std::find_if(v.begin(), v.end(), Max_7());
+    if(choose == v.end())
+    {
+        std::cout << "没有找到捏!" << "\n";
+    }
+    else
+    {
+        std::cout << "已经找到捏！ -- " << *choose << "\n";
+    }
+
+    std::cout << "还没有排序捏！\n";
+    for (int i = 0; i < 10;i++)
+    {
+        std::cout << "v[" << i << "] == " << v[i] << " ";
+    }
+
+    std::cout << "\n排序过后捏！\n";
+    std::sort(v.begin(), v.end(), MyCompare()); //二元谓词调用
+    //不想声明对象可以直接使用匿名对象
+    for (int i = 0; i < 10;i++)
+    {
+        std::cout << "v[" << i << "] == " << v[i] << " ";
+    }
+    return 0;
+}
+
+
+
+
+
