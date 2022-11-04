0d7c82a0d0664bd9f82bc187027a7b7ccb2e8dca
yuri2078 <yuri2078170658@gmail.com>
2022-11-04 22:11:46 +0800

    main

---
+++ leetcode/754.到达终点数字.cpp
@@ -0,0 +1,46 @@
+/*
+ * @lc app=leetcode.cn id=754 lang=cpp
+ *
+ * [754] 到达终点数字
+ */
+
+// @lc code=start
+#include <iostream>
+using namespace std;
+
+class Solution {
+public:
+	static int reachNumber(int target)
+	{
+		if (target < 0) {
+			return reachNumber(-target);
+		}
+
+		int ans = 0;
+		int k = 1;
+		while (ans < target) {
+			ans += k;
+			k++;
+		}
+		if ((ans - target) % 2 == 0) {
+			return k - 1;
+		}
+
+		if (target % 2 == 0 && (ans - k) % 2 == 0 || (ans + k - target) % 2 == 0) {
+			return k;
+		} else {
+			return k + 1;
+        }
+    }
+};
+
+
+
+
+// int main(int argc, const char **argv)
+// {
+// 	cout << Solution::reachNumber(39) << endl;
+//     return 0;
+// }
+// @lc code=end
+
