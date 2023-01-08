3f10ac89a38b8bb3132a7a83c55a09fe7c13e6b3
yuri2078 <yuri2078170658@gmail.com>
2023-01-08 19:10:59 +0800

    main

--- c++/test.cpp
+++ c++/test.cpp
@@ -1,13 +1,17 @@
 #include <iostream>
 int main(int argc, char const* argv[])
 {
-	int set[16]{1};
+	int set[18]{1};
 	for (int i = 1; set[i] <= 10000000; i++) {
 		set[i] = set[i - 1] * 3;
 	}
 	
 	for (int i = 0; i < 15; i++) {
 		std::cout << set[i] << "  ";
+	}
+
+	for (int i = 0; i < 100; i++) {
+		std::cout << i << " ";
 	}
 	std::cout << std::endl;
 	return 0;
