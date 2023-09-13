2be9c9fcada744b0418e9ead150fd2e5566751c5
yuri2078 <yuri2078170658@gmail.com>
2023-09-13 21:49:43 +0800

    main

--- c++/test.cpp
+++ c++/test.cpp
@@ -3,6 +3,7 @@
 
 int main() {
   info << std::numeric_limits<long unsigned int>::max();
-  info << std::numeric_limits<unsigned long int>::max();
+  info << std::numeric_limits<std::string::size_type>::max();
+
   return 0;
 }
